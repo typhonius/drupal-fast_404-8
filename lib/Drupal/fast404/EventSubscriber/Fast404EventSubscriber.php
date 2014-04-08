743c749c1713246636eea515cad9c540dcca8d30
Adam Malone <adam@adammalone.net>
2014-04-08 21:34:53 +1000

    Refactored a bunch of the module code so it can be loaded directly from settings.php without having to boot the kernel.

--- lib/Drupal/fast404/EventSubscriber/Fast404EventSubscriber.php
+++ lib/Drupal/fast404/EventSubscriber/Fast404EventSubscriber.php
@@ -2,59 +2,27 @@
 
 namespace Drupal\fast404\EventSubscriber;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Drupal\Component\Utility\Settings;
-use Drupal\Component\Utility\String;
 use Drupal\fast404\Fast404;
 
 class Fast404EventSubscriber implements EventSubscriberInterface {
-  public $event;
 
   public function onKernelRequest(GetResponseEvent $event) {
-    $this->event = $event;
-
     $request = \Drupal::service('request');
 
     $fast_404 = new Fast404($request);
-    $fast_404->extensionCheck();
 
+    $fast_404->extensionCheck();
     if ($fast_404->isPathBlocked()) {
-      $message = Settings::get('fast_404_html', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server (Fast 404).</p></body></html>');
-      $response = new Response(String::format($message, array('@path' => $request->getPathInfo())), 404);
-      $this->event->setResponse($response);
-      $this->event->stopPropagation();
+      $fast_404->response();
     }
 
     $fast_404->pathCheck();
-
     if ($fast_404->isPathBlocked()) {
-      drupal_set_message('path blocked');
+      $fast_404->response();
     }
-
-//    //require_once('fast404.inc');
-//
-//    // If the file extension wasn't set to be checked in settings.php, do it
-//    // here.
-//    if (!defined('FAST_404_EXT_CHECKED')) {
-//      // This function has an exit in it, so it will end the page if needed.
-//      fast_404_ext_check();
-//    }
-//
-//    // Don't do a full check if the path was already checked in settings.php.
-//    if (defined('FAST_404_PATH_CHECKED')) {
-//      return TRUE;
-//    }
-//
-//    // If the path is invalid then return the Fast 404 html
-//    fast_404_path_check();
-//
-//    if (Settings::get('fast_404_extension_check')) {
-//
-//    }
-    //$event->stopPropagation();
   }
 
   static function getSubscribedEvents() {
--- lib/Drupal/fast404/Fast404.php
+++ lib/Drupal/fast404/Fast404.php
@@ -1,28 +1,26 @@
 <?php
 
 namespace Drupal\fast404;
-use Drupal\Component\Utility\Settings;
-use Symfony\Component\HttpFoundation\Request;
 
+use Drupal\Component\Utility\Settings;
+use Drupal\Core\Database\Database;
+use Drupal\Component\Utility\String;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Fast404 {
 
-  public $extension_checked;
-
   public $respond_404 = FALSE;
 
-  public $path_checked;
-
   public $request;
+
+  public $event;
 
   public function __construct(Request $request) {
     $this->request = $request;
   }
 
-  // TODO set variables for whether it's checked or not
-
   public function extensionCheck() {
-
     // Get the path from the request.
     $path = $this->request->getPathInfo();
 
@@ -31,12 +29,12 @@ class Fast404 {
       return;
     }
 
-    // Check to see if the URL is an imagecache URL. If this file does not
-    // already exist, it will be handled via Drupal.
+    // Check to see if the URL is that of an image derivative.
+    // If this file does not already exist, it will be handled via Drupal.
     if (strpos($path, 'styles/')) {
 
       // Check to see if we will allow anon users to access this page.
-      if (!Settings::get('fast_404_allow_anon_imagecache', TRUE)) {
+      if (!Settings::get('fast404_allow_anon_imagecache', TRUE)) {
         $cookies = $this->request->cookies->all();
 
         // At this stage of the game we don't know if the user is logged in via
@@ -51,8 +49,8 @@ class Fast404 {
         }
       }
 
-      // We're allowing anyone to hit non-existing imagecache URLs (default
-      // behavior).
+      // We're allowing anyone to hit non-existing image derivative URLs
+      // (default behavior).
       else {
         return;
       }
@@ -62,7 +60,7 @@ class Fast404 {
     // whitelisted before running the extension check.
 
     // Check for exact URL matches and assume it's fine if we get one.
-    $url_whitelist = Settings::get('fast_404_url_whitelisting', FALSE);
+    $url_whitelist = Settings::get('fast404_url_whitelisting', FALSE);
     if (is_array($url_whitelist)) {
       if (in_array($path, $url_whitelist)) {
         return;
@@ -70,7 +68,7 @@ class Fast404 {
     }
 
     // Check for whitelisted strings in the URL.
-    $string_whitelist = Settings::get('fast_404_string_whitelisting', FALSE);
+    $string_whitelist = Settings::get('fast404_string_whitelisting', FALSE);
     if (is_array($string_whitelist)) {
       foreach ($string_whitelist as $str) {
         if (strstr($path, $str) !== FALSE) {
@@ -79,19 +77,52 @@ class Fast404 {
       }
     }
 
-    $extensions =  Settings::get('fast_404_exts', '/^(?!robots).*\.(txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i');
+    $extensions =  Settings::get('fast404_exts', '/^(?!robots).*\.(txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i');
     // Determine if URL contains a blacklisted extension.
     if (isset($extensions) && preg_match($extensions, $path, $m)) {
       $this->blockPath();
       return;
     }
 
-    $this->extension_checked = TRUE;
   }
 
   public function pathCheck() {
-    // if we have a database connection, use it.
-    $this->path_checked = TRUE;
+    // Since the path check is a lot more aggressive in its blocking we should
+    // actually check that the user wants it to be done.
+    if (!Settings::get('fast404_path_check', FALSE)) {
+      return;
+    }
+    // Get the path from the request.
+    $path = $this->request->getPathInfo();
+
+    // Ignore calls to the homepage, to avoid unnecessary processing.
+    if (!isset($path) || $path == '/') {
+      return;
+    }
+
+    // If we have a database connection we can use it, otherwise we might be
+    // initialising it.
+
+    // todo document that drupalkernel is after config atm but there is a d.o to make it before
+    // We remove '/' from the list of possible patterns as it exists in the router
+    // by default. This means that the query would match any path (/%) which is
+    // undesireable when we're only looking to match some paths.
+    $sql = "SELECT pattern_outline FROM {router} WHERE :path LIKE CONCAT(pattern_outline, '%') AND pattern_outline != '/'";
+    $result = Database::getConnection()->query($sql, array(':path' => $path))->fetchField();
+    if ($result) {
+      return;
+    }
+
+    // Check the URL alias table for anything that's not a standard Drupal path.
+    $sql = "SELECT pid FROM {url_alias} WHERE :alias = CONCAT('/', alias)";
+    $result = Database::getConnection()->query($sql, array(':alias' => $path))->fetchField();
+    if ($result) {
+      return;
+    }
+
+    // If we get to here it means nothing has matched the request so we assume
+    // it's a bad path and block it.
+    $this->blockPath();
 
   }
 
@@ -101,5 +132,11 @@ class Fast404 {
 
   public function isPathBlocked() {
     return $this->respond_404;
+  }
+
+  public function response() {
+    $message = Settings::get('fast404_html', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server (Fast 404).</p></body></html>');
+    $response = new Response(String::format($message, array('@path' => $this->request->getPathInfo())), 404);
+    $response->send();
   }
 }
